c05b17e362cdabc8484a93cd7ae50d03fdafc5f6
MeowOhMeow <abc22681157@gmail.com>
2024-04-05 22:35:29 +0800

    potential tasks

--- Debug/src/debugging/Debugging.java
+++ Debug/src/debugging/Debugging.java
@@ -6,10 +6,21 @@ import org.opencv.dnn.*;
 public class Debugging {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Net dnnNet = Dnn.readNet("./src/best.onnx");
 		System.out.println("Successly loaded!");
+
+		/* TODO:
+		 * 1. Load the image
+		 * 2. Preprocess the image
+		 * 3. Set the input
+		 * 4. Forward the image
+		 * 5. Get the output
+		 * 6. Postprocess the output
+		 * 
+		 * Note: please follow the java naming convention, 
+		 * and you can make private methods or class to make the code more readable.
+		*/
 	}
 
 }
