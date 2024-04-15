37a7a055cd030d9784293ff85b6b55f92ccc5bec
V20000000000000 <kevinkang5880@gmail.com>
2024-04-15 23:22:45 +0800

    add base graph

---
+++ basic_structure/graph/graph/Graph.java
@@ -0,0 +1,106 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph<T, U> {
+	private List<Map<Integer, Float>> adjacencyList;
+	private List<Map<Integer, EdgeProperty<U>>> edgePropertiesMap;
+	private EdgeProperty<U> emptyEdgeProperty;
+	private VertexProperty<T> emptyVertexProperty;
+	private List<VertexProperty<T>> vertexPropertiesMap;
+
+	public Graph(int numNodes) {
+		adjacencyList = new ArrayList<>();
+		vertexPropertiesMap = new ArrayList<>();
+		edgePropertiesMap = new ArrayList<>();
+		emptyVertexProperty = new VertexProperty<>(null);
+		emptyEdgeProperty = new EdgeProperty<>(null);
+		for (int i = 0; i < numNodes; i++) {
+			adjacencyList.add(new HashMap<>());
+			vertexPropertiesMap.add(emptyVertexProperty);
+			edgePropertiesMap.add(new HashMap<>());
+		}
+	}
+
+	public void addBidirectedEdge(int source, int target, float weight1, float weight2) {
+		addDirectedEdge(source, target, weight1);
+		addDirectedEdge(target, source, weight2);
+	}
+
+	public void addBidirectedEdge(Vertex source, Vertex target, float weight1, float weight2) {
+		addBidirectedEdge(source.getId(), target.getId(), weight1, weight2);
+	}
+	
+	public void addUndirectedEdge(int source, int target, float weight) {
+		addBidirectedEdge(source, target, weight, weight);
+	}
+	
+	public void addUndirectedEdge(Vertex source, Vertex target, float weight) {
+		addUndirectedEdge(source.getId(), target.getId(), weight);
+	}
+
+	public void addDirectedEdge(int source, int target, float weight) {
+		adjacencyList.get(source).put(target, weight);
+	}
+
+	public void addDirectedEdge(Vertex source, Vertex target, float weight) {
+		addDirectedEdge(source.getId(), target.getId(), weight);
+	}
+
+	public EdgeProperty<U> getEdgeProperty(int source, int target) {
+		return edgePropertiesMap.get(source).get(target);
+	}
+	
+	public EdgeProperty<U> getEdgeProperty(Vertex source, Vertex target) {
+		return getEdgeProperty(source.getId(), target.getId());
+	}
+
+	public float getEdgeWeight(int source, int target) {
+		return adjacencyList.get(source).get(target);
+	}
+
+	public float getEdgeWeight(Vertex source, Vertex target) {
+		return getEdgeWeight(source.getId(), target.getId());
+	}
+
+	public List<Integer> getNeighbors(int vertex) {
+		return new ArrayList<>(adjacencyList.get(vertex).keySet());
+	}
+
+	public List<Integer> getNeighbors(Vertex vertex) {
+		return getNeighbors(vertex.getId());
+	}
+
+	public VertexProperty<T> getVertexProperty(int vertex) {
+		return vertexPropertiesMap.get(vertex);
+	}
+
+	public VertexProperty<T> getVertexProperty(Vertex vertex) {
+		return getVertexProperty(vertex.getId());
+	}
+
+	public void setEdgeProperty(int source, int target, EdgeProperty<U> property) {
+		if (property.notEquals(emptyEdgeProperty)) {
+			edgePropertiesMap.get(source).put(target, property);
+		}
+	}
+	
+	public void setEdgeProperty(Vertex source, Vertex target, EdgeProperty<U> property) {
+		setEdgeProperty(source.getId(), target.getId(), property);
+	}
+
+	public void setVertexProperty(int vertex, VertexProperty<T> property) {
+		vertexPropertiesMap.set(vertex, property);
+	}
+
+	public void setVertexProperty(Vertex vertex, VertexProperty<T> property) {
+		setVertexProperty(vertex.getId(), property);
+	}
+
+	public int size() {
+		return adjacencyList.size();
+	}
+}
