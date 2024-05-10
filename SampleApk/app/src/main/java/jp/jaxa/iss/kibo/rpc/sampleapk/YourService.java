529011b3ce6a3c57d82ff5b7fd2c87a3345f793d
MeowOhMeow <abc22681157@gmail.com>
2024-05-10 22:29:12 +0800

    refactor

--- SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -3,26 +3,23 @@ package jp.jaxa.iss.kibo.rpc.sampleapk;
 import android.util.Log;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
-import  jp.jaxa.iss.kibo.rpc.sampleapk.Debugging_new;
 
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import gov.nasa.arc.astrobee.Kinematics;
 
-
-
 import org.opencv.core.Mat;
 
-
 /**
- * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
+ * Class meant to handle commands from the Ground Data System and execute them
+ * in Astrobee.
  */
 
 public class YourService extends KiboRpcService {
     private Point[] areaPoints = new Point[4];
     private Quaternion[] areaQuaternions = new Quaternion[4];
-    private static String MapOfItem(int index)
-    {
+
+    private static String MapOfItem(int index) {
         String itemName = "";
         switch (index) {
             case 0:
@@ -63,11 +60,11 @@ public class YourService extends KiboRpcService {
     }
 
     public YourService() {
-        areaPoints[0] = new Point(10.9078d,-10.0293d,5.1124d);
+        areaPoints[0] = new Point(10.9078d, -10.0293d, 5.1124d);
         areaPoints[1] = new Point(10.8828d, -8.7924d, 4.3904d);
         areaPoints[2] = new Point(10.8828d, -7.8424d, 4.4091d);
         areaPoints[3] = new Point(10.5280d, -6.7699d, 4.9872d);
-        areaQuaternions[0] = new Quaternion(0.707f,-0.707f,0f, 0f);
+        areaQuaternions[0] = new Quaternion(0.707f, -0.707f, 0f, 0f);
         areaQuaternions[1] = new Quaternion(-0.5f, 0.5f, 0.5f, 0.5f);
         areaQuaternions[2] = new Quaternion(-0.5f, 0.5f, 0.5f, 0.5f);
         areaQuaternions[3] = new Quaternion(0f, 0.707f, 0.707f, 0f);
@@ -90,28 +87,22 @@ public class YourService extends KiboRpcService {
         }
 
         // Get a camera image.
-        Log.i("CHIPI-CHIPI", "begin " );
+        Log.i("CHIPI-CHIPI", "begin of inference");
         Mat image = api.getMatNavCam();
-        Debugging_new debug=new Debugging_new();
-        int[] count = new int[10];
-        count=debug.image(image);
-        for(int i = 0; i < count.length; ++i)
-        {
-            if(count[i] != 0)
+        int[] count = YOLOInference.getPredictions(image);
+        for (int i = 0; i < count.length; ++i) {
+            if (count[i] != 0)
                 api.setAreaInfo(area, MapOfItem(i), count[i]);
         }
-        Log.i("CHIPI-CHIPI", "end " );
+        Log.i("CHIPI-CHIPI", "end of inference");
 
         api.saveMatImage(image, "Area" + area + ".jpg");
-
 
         api.flashlightControlFront(0f);
     }
 
-
-
     @Override
-    protected void runPlan1(){
+    protected void runPlan1() {
         // The mission starts.
         api.startMission();
 
@@ -126,13 +117,24 @@ public class YourService extends KiboRpcService {
         api.moveTo(new Point(10.56d, -7.4d, 4.62d), new Quaternion(), true);
         goToTakeAPic(3);
 
-        //Log.i();
-        //here?
+        // When you move to the front of the astronaut, report the rounding completion.
+        api.reportRoundingCompletion();
+
+        /* ********************************************************** */
+        /* Write your code to recognize which item the astronaut has. */
+        /* ********************************************************** */
+
+        // Let's notify the astronaut when you recognize it.
+        api.notifyRecognitionItem();
+
+        /*
+         * Write your code to move Astrobee to the location of the target item (what the
+         * astronaut is looking for)
+         */
+
+        // Take a snapshot of the target item.
         api.takeTargetItemSnapshot();
-        //where?
 
     }
-    
 
-   
 }
