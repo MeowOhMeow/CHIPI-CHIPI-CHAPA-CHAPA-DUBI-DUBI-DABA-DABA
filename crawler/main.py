72378f7aa69bcab54888ba1d69a0b73c5b6e0c33
MeowOhMeow <abc22681157@gmail.com>
2024-07-20 07:15:44 +0800

    enhance stability by directly trigger by console

--- crawler/main.py
+++ crawler/main.py
@@ -89,13 +89,7 @@ def upload_to_slot(driver: webdriver.Edge, slot_id: int, file_path: str):
         f"/html/body/div/div/main/div/div/div[2]/div[2]/div[{slot_id}]/div[2]/button[1]"
     )
     start_button = driver.find_element(By.XPATH, start_button_xpath)
-    driver.execute_script("arguments[0].scrollIntoView(true);", start_button)
-
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, start_button_xpath))
-    )
-    time.sleep(0.5)
-    start_button.click()
+    driver.execute_script("arguments[0].click();", start_button)
 
     # Confirm simulation
     confirm_button_xpath = "/html/body/div/div/main/div/div/div[2]/div[2]/div[3]/div[2]/div/div/div/form/div[2]/button[1]"
@@ -159,12 +153,7 @@ def download_files(driver: webdriver.Edge):
     log_button = driver.find_element(By.XPATH, log_file_xpath)
     image_button = driver.find_element(By.XPATH, image_file_xpath)
 
-    driver.execute_script("arguments[0].scrollIntoView(true);", log_button)
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, log_file_xpath))
-    )
-    time.sleep(0.5)
-    log_button.click()
+    driver.execute_script("arguments[0].click();", log_button)
     time.sleep(0.5)
     # get status of the image button
     status = image_button.get_attribute("class")
@@ -172,7 +161,7 @@ def download_files(driver: webdriver.Edge):
         time.sleep(0.5)
         status = image_button.get_attribute("class")
     time.sleep(0.5)
-    image_button.click()
+    driver.execute_script("arguments[0].click();", image_button)
     time.sleep(0.5)
     status = image_button.get_attribute("class")
     while "disabled" in status:
@@ -199,12 +188,7 @@ def view_result_and_reupload(driver: webdriver.Edge, config: list):
             if slot_status == "finished":
                 view_button_xpath = f"/html/body/div/div/main/div/div/div[2]/div[2]/div[{index}]/div[2]/button[3]"
                 view_button = driver.find_element(By.XPATH, view_button_xpath)
-                driver.execute_script("arguments[0].scrollIntoView(true);", view_button)
-                WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, view_button_xpath))
-                )
-                time.sleep(0.5)
-                view_button.click()
+                driver.execute_script("arguments[0].click();", view_button)
 
                 download_files(driver)
                 remove_simulation(driver)
@@ -226,6 +210,16 @@ if __name__ == "__main__":
     driver = webdriver.Edge()
     try:
         login(driver, config)
+        driver.get("https://d392k6hrcntwyp.cloudfront.net/simulation/results")
+        view_button_xpath = "/html/body/div/div/main/div/div/div[2]/div[3]/table/tbody/tr[1]/td[5]/button[1]"
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, view_button_xpath))
+        )
+        driver.find_element(By.XPATH, view_button_xpath).click()
+        download_files(driver)
+        time.sleep(5)
+
+        driver.get("https://d392k6hrcntwyp.cloudfront.net/simulation")
         start_simulation(driver, config)
         view_result_and_reupload(driver, config)
         # driver.get("https://d392k6hrcntwyp.cloudfront.net/simulation/results")
