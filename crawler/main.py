a6f635c488e4b7f9ede06c7f4c18c003461490fb
MeowOhMeow <abc22681157@gmail.com>
2024-07-24 00:25:10 +0800

    我就不信了

--- crawler/main.py
+++ crawler/main.py
@@ -181,16 +181,14 @@ def remove_simulation(driver: webdriver.Edge):
     )
     confirm_button = driver.find_element(By.XPATH, confirm_button_xpath)
     driver.execute_script("arguments[0].click();", confirm_button)
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div/div/main/div/div/div[2]/div[3]/div/div/div/form")
+    time.sleep(0.5)
+    # if modal is not closed, wait until it is closed
+    if len(driver.find_elements(By.XPATH, "/html/body/div/div/main/div/div/div[2]/div[3]/div/div/div/form")) > 0:
+        WebDriverWait(driver, 100).until(
+            EC.invisibility_of_element_located(
+                (By.XPATH, "/html/body/div/div/main/div/div/div[2]/div[3]/div/div/div/form")
+            )
         )
-    )
-    WebDriverWait(driver, 100).until(
-        EC.invisibility_of_element_located(
-            (By.XPATH, "/html/body/div/div/main/div/div/div[2]/div[3]/div/div/div/form")
-        )
-    )
 
 
 def download_files(driver: webdriver.Edge, index: int, html_folder: str):
